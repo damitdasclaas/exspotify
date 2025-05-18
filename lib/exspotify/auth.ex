51710c52f3a5c2b7e787166e8cbf5659589cc860
Claas Thore Klein <>
2025-05-18 23:34:16 +0200

    Add functions to build authorization URL and exchange authorization code for access token in Exspotify.Auth module

--- lib/exspotify/auth.ex
+++ lib/exspotify/auth.ex
@@ -33,5 +33,64 @@ defmodule Exspotify.Auth do
     end
   end
 
+  def build_authorization_url(scopes, state \\ nil) do
+    client_id = Keyword.get(config(), :client_id)
+    redirect_uri = Keyword.get(config(), :redirect_uri)
+    scope_string = Enum.join(scopes, " ")
+
+    query_params = %{
+      client_id: client_id,
+      response_type: "code",
+      redirect_uri: redirect_uri,
+      scope: scope_string
+    }
+
+    query_params =
+      if state do
+        Map.put(query_params, :state, state)
+      else
+        query_params
+      end
+
+    uri = URI.parse("https://accounts.spotify.com/authorize")
+    {:ok, %{uri | query: URI.encode_query(query_params)}}
+  end
+
+  def exchange_code_for_token(code) do
+    client_id = Keyword.get(config(), :client_id)
+    client_secret = Keyword.get(config(), :client_secret)
+    redirect_uri = Keyword.get(config(), :redirect_uri)
+
+    auth_string = Base.encode64("#{client_id}:#{client_secret}")
+
+    headers = [
+      {"Authorization", "Basic #{auth_string}"},
+      {"Content-Type", "application/x-www-form-urlencoded"}
+    ]
+
+    form_body = %{
+      "grant_type" => "authorization_code",
+      "code" => code,
+      "redirect_uri" => redirect_uri
+    }
+
+    case Req.post("https://accounts.spotify.com/api/token", form: form_body, headers: headers) do
+      {:ok, %Req.Response{status: 200, body: decoded_body}} ->
+        if is_map(decoded_body) and Map.has_key?(decoded_body, "access_token") do
+          {:ok, decoded_body}
+        else
+          # Attempt to decode if it's a JSON string
+          case Jason.decode(decoded_body) do
+            {:ok, %{"access_token" => _token} = fully_decoded_body} -> {:ok, fully_decoded_body}
+            _ -> {:error, "Failed to parse token response or unexpected format after code exchange"}
+          end
+        end
+      {:ok, %Req.Response{status: status, body: body}} ->
+        {:error, "Failed to exchange code for token: Status #{status}, Body: #{body}"}
+      {:error, exception} ->
+        {:error, "HTTP request failed during code exchange: #{exception.reason}"}
+    end
+  end
+
   # ... other auth functions for different flows, token refresh, etc.
 end
