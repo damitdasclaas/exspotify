1478fc7966c216c22c6e506bf3776e42d2f4412c
Claas Thore Klein <>
2025-05-23 12:42:06 +0200

    Add Exspotify.Users module for interacting with Spotify Users API endpoints, including functions to retrieve user profiles, manage followed artists and users, and fetch top items with pagination support.

---
+++ lib/exspotify/users.ex
@@ -0,0 +1,113 @@
+defmodule Exspotify.Users do
+  @moduledoc """
+  Provides functions for interacting with the Users endpoints of the Spotify Web API.
+  See: https://developer.spotify.com/documentation/web-api/reference/users-profile
+  """
+
+  alias Exspotify.Client
+  alias Exspotify.Pagination
+
+  @doc """
+  Get the current user's profile.
+  https://developer.spotify.com/documentation/web-api/reference/get-current-users-profile
+  """
+  @spec get_current_user_profile(String.t()) :: any
+  def get_current_user_profile(token) do
+    Client.get("/me", [], token)
+  end
+
+  @doc """
+  Get a user's public profile by their Spotify user ID.
+  https://developer.spotify.com/documentation/web-api/reference/get-users-profile
+  """
+  @spec get_user_profile(String.t(), String.t()) :: any
+  def get_user_profile(user_id, token) do
+    Client.get("/users/#{user_id}", [], token)
+  end
+
+  @doc """
+  Get the current user's top artists or tracks (paginated).
+  https://developer.spotify.com/documentation/web-api/reference/get-users-top-artists-and-tracks
+  type: "artists" or "tracks"
+  """
+  @spec get_user_top_items(String.t(), String.t(), keyword) :: any
+  def get_user_top_items(type, token, opts \\ []) do
+    query = URI.encode_query(opts)
+    path = "/me/top/#{type}" <> if(query != "", do: "?#{query}", else: "")
+    Client.get(path, [], token)
+  end
+
+  @doc """
+  Fetches all top items (artists or tracks) for the user, following all pages.
+
+  **Warning:** This may make a large number of requests if the user has many top items.
+  You can limit the number of items fetched with the `:max_items` option (default: 200).
+  """
+  @spec get_all_user_top_items(String.t(), String.t(), keyword) :: [map]
+  def get_all_user_top_items(type, token, opts \\ []) do
+    max_items = Keyword.get(opts, :max_items, 200)
+    fetch_page = fn page_opts -> get_user_top_items(type, token, page_opts) end
+    Pagination.fetch_all(fetch_page, opts, max_items)
+  end
+
+  @doc """
+  Follow artists or users.
+  **Warning:** This will change the user's followed artists or users.
+  type: "artist" or "user"
+  https://developer.spotify.com/documentation/web-api/reference/follow-artists-users
+  """
+  @spec follow_artists_or_users(String.t(), String.t(), [String.t()], String.t()) :: any
+  def follow_artists_or_users(type, token, ids, _context_user_id \\ nil) when is_list(ids) do
+    query = URI.encode_query(%{"type" => type, "ids" => Enum.join(ids, ",")})
+    Client.put("/me/following?#{query}", %{}, [], token)
+  end
+
+  @doc """
+  Unfollow artists or users.
+  **Warning:** This will change the user's followed artists or users.
+  type: "artist" or "user"
+  https://developer.spotify.com/documentation/web-api/reference/unfollow-artists-users
+  """
+  @spec unfollow_artists_or_users(String.t(), String.t(), [String.t()], String.t()) :: any
+  def unfollow_artists_or_users(type, token, ids, _context_user_id \\ nil) when is_list(ids) do
+    query = URI.encode_query(%{"type" => type, "ids" => Enum.join(ids, ",")})
+    Client.delete("/me/following?#{query}", [], token)
+  end
+
+  @doc """
+  Check if the current user follows one or more artists or users.
+  type: "artist" or "user"
+  https://developer.spotify.com/documentation/web-api/reference/check-current-user-follows
+  """
+  @spec check_if_user_follows_artists_or_users(String.t(), String.t(), [String.t()], String.t()) :: any
+  def check_if_user_follows_artists_or_users(type, token, ids, _context_user_id \\ nil) when is_list(ids) do
+    query = URI.encode_query(%{"type" => type, "ids" => Enum.join(ids, ",")})
+    Client.get("/me/following/contains?#{query}", [], token)
+  end
+
+  @doc """
+  Get the current user's followed artists (paginated).
+  https://developer.spotify.com/documentation/web-api/reference/get-followed-artists
+  """
+  @spec get_followed_artists(String.t(), keyword) :: any
+  def get_followed_artists(token, opts \\ []) do
+    query = URI.encode_query(opts)
+    path = "/me/following?type=artist" <> if(query != "", do: "&#{query}", else: "")
+    case Client.get(path, [], token) do
+      {:ok, %{"artists" => artists}} -> {:ok, artists}
+      other -> other
+    end
+  end
+
+  @doc """
+  Fetches all followed artists for the user, following all pages.
+  **Warning:** This may make a large number of requests if the user follows many artists.
+  You can limit the number of items fetched with the `:max_items` option (default: 200).
+  """
+  @spec get_all_followed_artists(String.t(), keyword) :: [map]
+  def get_all_followed_artists(token, opts \\ []) do
+    max_items = Keyword.get(opts, :max_items, 200)
+    fetch_page = fn page_opts -> get_followed_artists(token, page_opts) end
+    Pagination.fetch_all(fetch_page, opts, max_items)
+  end
+end
